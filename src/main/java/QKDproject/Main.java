daaad00d22c97739e78815fb522d29e5dd2caac2
Marc <Marc@MARC-PC>
2021-02-24 12:06:52 -0500

    initial commit

---
+++ src/main/java/QKDproject/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package QKDproject;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Marc
+ */
+public class Main extends Application {
+	@Override // Override the start method in the Application class
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("ChatMockup.fxml"));
+    
+    Scene scene = new Scene(root);
+    stage.setTitle("Login");
+    stage.setScene(scene);
+    stage.show();
+  }
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
