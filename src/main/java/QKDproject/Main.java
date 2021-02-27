ec3c329564a4210743114651215b2acae4ad88b9
Marc <Marc@MARC-PC>
2021-02-26 20:32:44 -0500

    made a ChatController class and added my qkd code (it's not integrated w Java yet)

--- src/main/java/QKDproject/Main.java
+++ src/main/java/QKDproject/Main.java
@@ -7,23 +7,30 @@ package QKDproject;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 /**
  *
  * @author Marc
  */
 public class Main extends Application {
+	
 	@Override // Override the start method in the Application class
   public void start(Stage stage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("ChatMockup.fxml"));
     
     Scene scene = new Scene(root);
-    stage.setTitle("Login");
+    stage.setTitle("Chat test");
     stage.setScene(scene);
     stage.show();//test
+	
+	
   }
   
   public static void main(String[] args) {
