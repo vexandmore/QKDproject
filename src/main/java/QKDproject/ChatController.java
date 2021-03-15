d254f6c2944d96112021b76c59bb7626b7edeb27
Marc <Marc@MARC-PC>
2021-03-15 17:51:49 -0400

    make gridpane columns scale properly

--- src/main/java/QKDproject/ChatController.java
+++ src/main/java/QKDproject/ChatController.java
@@ -32,25 +32,18 @@ public class ChatController {
 	public void initialize() {
 		chatGrid.setVgap(10);
 		chatGrid.setPadding(new Insets(0, 0, 30, 0));
-		
-		ColumnConstraints a = new ColumnConstraints();
-		a.setPercentWidth(50);
-		ColumnConstraints b = new ColumnConstraints();
-		b.setPercentWidth(50);
-		chatGrid.getColumnConstraints().addAll(a, b);
 	}
 	@FXML
 	public void sendMessage() {
+		//make and add chat bubble
 		ChatBubble newBubble = new ChatBubble(textfield.getText(), chatGrid.widthProperty());
-		int column = numberMessages % 2;
+		int column = (numberMessages+1) % 2;
 		chatGrid.add(newBubble, column, numberMessages++);
-		
 		RowConstraints constraint = new RowConstraints();
 		constraint.setMinHeight(newBubble.getBHeight());
 		constraint.setVgrow(Priority.NEVER);
-		
 		chatGrid.getRowConstraints().add(constraint);
-		
+		//scroll to bottom
 		scrollPane.vvalueProperty().set(scrollPane.getVmax());
 	}
 }
@@ -76,6 +69,7 @@ class ChatBubble extends StackPane {
 		
 		bubble.heightProperty().set(this.text.boundsInLocalProperty().getValue().getHeight());
 		this.getChildren().addAll(bubble, this.text);
+		//this.setAlignment(Pos.CENTER_RIGHT);
 	}
 	
 	public double getBHeight() {
