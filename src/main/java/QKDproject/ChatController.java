615fd8d89f938c1ba7badfaa72ccf520859860e0
Marc <Marc@MARC-PC>
2021-03-22 11:59:48 -0400

    added indicator on state of message sending

--- src/main/java/QKDproject/ChatController.java
+++ src/main/java/QKDproject/ChatController.java
@@ -9,6 +9,7 @@ import javafx.application.Platform;
 import javafx.beans.property.*;
 import javafx.fxml.FXML;
 import javafx.geometry.*;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -17,7 +18,7 @@ import javafx.scene.text.Text;
 import QKDproject.exception.*;
 
 /**
- *
+ * Controller for the Chat window.
  * @author Marc
  */
 public class ChatController {
@@ -25,44 +26,52 @@ public class ChatController {
 	@FXML public ScrollPane scrollPane;
 	@FXML public TextField textfield;
 	@FXML public BorderPane pane;
-	private int numberMessages = 0;
+	private int currentRow = 0;
 	private Chat chat;
 	
 	public ChatController() {
 	}
 	
+	/**
+	 * Called by the Chat so that this can send messages to Chat and Chat can 
+	 * notify this of new messages.
+	 * @param c Chat instance.
+	 */
 	protected void setChat(Chat c) {
 		this.chat = c;
 	}
 	
 	private void initialize() {
 		chatGrid.setVgap(10);
-		chatGrid.setPadding(new Insets(0, 5, 30, 0));
+		chatGrid.setPadding(new Insets(30, 30, 30, 30));
 	}
+	
 	@FXML
 	private void sendMessage() {
 		//make and add chat bubble
 		ChatBubble newBubble = new ChatBubble(textfield.getText(), chatGrid.widthProperty());
-		//int column = (numberMessages+1) % 2;
 		int column = 1;
-		chatGrid.add(newBubble, column, numberMessages++);
+		chatGrid.add(newBubble, column, currentRow++);
 		RowConstraints constraint = new RowConstraints();
 		constraint.prefHeightProperty().bind(newBubble.heightProperty());
 		constraint.setVgrow(Priority.NEVER);
 		chatGrid.getRowConstraints().add(constraint);
 		//scroll to bottom
 		scrollPane.vvalueProperty().set(scrollPane.getVmax());
-		
-		//send message to other asynchronously
+		//make and add progress indicator
+		ChatIndicator i = new ChatIndicator(ChatIndicator.Progress.STARTED);
+		chatGrid.add(i, column, currentRow++);
+		//send message to other chatter asynchronously
 		Runnable r = () -> {
 			try {
 				chat.sendMessage(textfield.getText());
+				i.setProgress(ChatIndicator.Progress.SENT);
 			} catch (Exception t) {
 				showError(t);
+				i.setProgress(ChatIndicator.Progress.FAILED);
 			}
 		};
 		new Thread(r).start();
-		//Platform.runLater(r);
 	}
 	
 	private void showError(Throwable t) {
@@ -97,7 +106,7 @@ public class ChatController {
 				//make and add chat bubble
 				ChatBubble newBubble = new ChatBubble(message, chatGrid.widthProperty());
 				int column = 0;
-				chatGrid.add(newBubble, column, numberMessages++);
+				chatGrid.add(newBubble, column, currentRow++);
 				RowConstraints constraint = new RowConstraints();
 				constraint.prefHeightProperty().bind(newBubble.heightProperty());
 				constraint.setVgrow(Priority.NEVER);
@@ -118,6 +127,10 @@ public class ChatController {
 	}
 }
 
+/**
+ * Represents a chat bubble, ie text in a rectangle.
+ * @author Marc
+ */
 class ChatBubble extends StackPane {
 	private Rectangle bubble;
 	private Text text;
@@ -140,5 +153,53 @@ class ChatBubble extends StackPane {
 		this.getChildren().addAll(bubble, this.text);
 		
 		this.setPadding(new Insets(5,5,5,5));
+	}
+}
+
+/**
+ * Represents progress on whether the message has been sent.
+ * @author Marc
+ */
+class ChatIndicator extends StackPane {
+	enum Progress {
+		STARTED, SENT, FAILED
+	}
+	
+	private Progress progress;
+	private Text indicator;
+	
+	public ChatIndicator() {
+		this(Progress.STARTED);
+	}
+	
+	public ChatIndicator(Progress initialProgress) {
+		indicator = new Text();
+		this.getChildren().add(indicator);
+		this.setAlignment(Pos.CENTER_RIGHT);
+		setProgress(initialProgress);
+	}
+	
+	/**
+	 * Updates the progress display. Can be called outside the main GUI thread.
+	 * @param newProgress 
+	 */
+	public void setProgress(Progress newProgress) {
+		progress = newProgress;
+		updateProgress();
+	}
+	
+	private void updateProgress() {
+		Platform.runLater(() -> {
+			switch (progress) {
+				case STARTED:
+					indicator.setText("Sending...");
+					break;
+				case SENT:
+					indicator.setText("Sent.");
+					break;
+				case FAILED:
+					indicator.setText("Message failed to send");
+			}
+		});
 	}
 }
